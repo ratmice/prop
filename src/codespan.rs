2aff79678cc5a72abf688b36a7b55ac6e6f8dd88
matt rice <ratmice@gmail.com>
2020-03-25 15:41:36 -0700

    work on error messages a bit

--- src/codespan.rs
+++ src/codespan.rs
@@ -9,23 +9,47 @@ pub fn codespan<'a>(
     error: &Error<'a>,
 ) -> (SimpleFiles<&'a str, &'a str>, Diagnostic<usize>) {
     use lalrpop_util::ParseError::*;
+
     let mut files = SimpleFiles::new();
     let file_id = files.add(filename, data);
+
+    // fn join_expected(v: &Vec<String>) -> String {
+    let join_expected = |v: &Vec<String>| {
+        let len = v.len();
+        //  let get_string = |v: &Vec<String>, i: usize| v[i].to_string();
+        let get_string = |v: &Vec<String>, i: usize| unsafe { v.get_unchecked(i).to_string() };
+        if len == 0 {
+            String::new()
+        } else if len == 1 {
+            get_string(v, 0)
+        } else {
+            format!(
+                "{} or {}",
+                v[0..(len - 2)].join(", "),
+                get_string(v, len - 1)
+            )
+        }
+    };
+
+    let fmt_expected = |expected| format!("Expected: {}", join_expected(expected));
+
     let diag = match error {
         InvalidToken { location } => Diagnostic::error()
-            .with_message("Invalid Token")
+            .with_message("Invalid token")
             .with_labels(vec![Label::primary(file_id, *location..*location)]),
         UnrecognizedEOF { location, expected } => Diagnostic::error()
-            .with_message("Unrecognized EOF")
-            .with_labels(vec![Label::primary(file_id, *location..*location)])
-            .with_message(expected.join(" ")),
+            .with_message("Unexpected EOF")
+            .with_labels(vec![
+                Label::primary(file_id, *location..*location).with_message(fmt_expected(expected))
+            ]),
         UnrecognizedToken {
             token: (start, _tok, end),
             expected,
         } => Diagnostic::error()
-            .with_message("Unrecognized Token")
-            .with_labels(vec![Label::primary(file_id, *start..*end)])
-            .with_message(expected.join(" ")),
+            .with_message("Unrecognized token")
+            .with_labels(vec![
+                Label::primary(file_id, *start..*end).with_message(fmt_expected(expected))
+            ]),
         ExtraToken {
             token: (start, _tok, end),
         } => Diagnostic::error()
@@ -33,7 +57,7 @@ pub fn codespan<'a>(
             .with_labels(vec![Label::primary(file_id, *start..*end)])
             .with_message("Extra token"),
         User { error } => Diagnostic::error()
-            .with_message("Invalid Token")
+            .with_message("Invalid token")
             .with_labels(vec![Label::primary(file_id, error.0.clone())])
             .with_message("Invalid token"),
     };
